683cc858f18b37b37f4bad83782e6606ed8e58c0
mozolev <avmozolev@yandex.ru>
2016-06-05 13:09:15 +0300

    cookies :construction:

--- src/form.js
+++ src/form.js
@@ -2,6 +2,7 @@
 
 (function() {
   var MAX_RATING = 3;
+  var cookies = require('browser-cookies');
   var formContainer = document.querySelector('.overlay-container');
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
@@ -26,8 +27,31 @@
     formContainer.classList.add('invisible');
   };
 
+  // При отправки формы, сохраняем куки.
+  form.onsubmit = function() {
+    // Текущая дата.
+    var nowDate = new Date();
+    // Дата дня рождения в этом году.
+    var myLastBirthdayDate = new Date(nowDate.getFullYear() + '-05-23');
+    // Если текущая дата меньше даты дня рождения, то берем день рождения годом ранее.
+    if (nowDate < myLastBirthdayDate) {
+      myLastBirthdayDate.setFullYear(myLastBirthdayDate.getFullYear() - 1);
+    }
+    // Количество дней с прошлого дня рождения.
+    var daysAfterLastBirthday = nowDate - myLastBirthdayDate;
+    // Дата окончания куки.
+    var cookiesOptions = {expires: new Date(Date.now() + daysAfterLastBirthday)};
+    // Записываем куку name.
+    cookies.set('name', name.value, cookiesOptions);
+    // Записываем куку rating.
+    cookies.set('rating', marks.value, cookiesOptions);
+  };
 
   name.required = true;
+  // Если есть кука для name, добавляем в поле name.
+  name.value = cookies.get('name') || '';
+  // Если есть кука для rating, то добавляем checked для оценки.
+  marks.value = cookies.get('rating') || marks.value;
   name.addEventListener('input', checkInputs);
   text.addEventListener('input', checkInputs);
   createAlert(name);
@@ -44,14 +68,20 @@
     });
   }
 
-
+  /**
+   * Создание сообщение под полем ввода.
+   * @param  {object}  input  Инпут для которого создаем сообщение.
+   */
   function createAlert(input) {
     var elemAlert = document.createElement('div');
     elemAlert.classList.add(input.name + '-alert');
     input.insertAdjacentElement('afterEnd', elemAlert);
   }
 
-
+  /**
+   * Изменение сообщения под полем ввода.
+   * @param  {object}  input  Инпут под которым нужно изменить сообщение.
+   */
   function inputAlertChange(input) {
     var alert = document.querySelector('.' + input.name + '-alert');
     alert.innerHTML = input.validationMessage;
