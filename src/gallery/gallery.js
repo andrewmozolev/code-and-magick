aa721e807569362fa8f040d07851340d581966b9
mozolev <avmozolev@yandex.ru>
2016-07-02 21:12:22 +0300

    add gallery location hash :construction:

--- src/gallery/gallery.js
+++ src/gallery/gallery.js
@@ -5,16 +5,18 @@ var Gallery = require('./gallery-show-image');
 var photogalleryImages = document.querySelectorAll('.photogallery-image img');
 var photogallery = document.querySelector('.photogallery');
 
-var arraySrc = Array.prototype.map.call(photogalleryImages, function(image) {
-  return image.src;
+var arrayPathPictures = Array.prototype.map.call(photogalleryImages, function(image) {
+  return image.getAttribute('src');
 });
 
-var gallery = new Gallery(arraySrc);
+var gallery = new Gallery(arrayPathPictures);
 
 photogallery.addEventListener('click', function(evt) {
-  var imageNumber = arraySrc.indexOf(evt.target.src);
+  var imageNumber = arrayPathPictures.indexOf(evt.target.getAttribute('src'));
   if (imageNumber >= 0) {
     evt.preventDefault();
-    gallery.show(imageNumber);
+    gallery.updateHash(imageNumber);
   }
 });
+
+window.addEventListener('load', gallery.hashchange);
