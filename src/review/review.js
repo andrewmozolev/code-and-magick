aa6ebf61db0f1072edf44b1332b5eb27f521c9a9
mozolev <avmozolev@yandex.ru>
2016-07-04 17:17:10 +0300

    Контексты и прототипы :construction:

--- src/review/review.js
+++ src/review/review.js
@@ -10,15 +10,25 @@ var getReviewsElement = require('./get-review-element.js');
 var Review = function(data, container) {
   this.data = data;
   this.element = getReviewsElement(this.data);
-  this.onReviewClick = function() {
-    this.classList.add('review-quiz-answer-active');
-  };
-  this.remove = function() {
-    this.element.removeEventListener('click', this.onReviewClick);
-    this.element.parentNode.removeChild(this.element);
-  };
+  this.onReviewClick = this.onReviewClick.bind(this);
+  this.remove = this.remove.bind(this);
   this.element.addEventListener('click', this.onReviewClick);
   container.appendChild(this.element);
+};
+
+Review.prototype.onReviewClick = function(evt) {
+  if (evt.target.classList.contains('review-quiz-answer')) {
+    if (this.activeAnsver) {
+      this.activeAnsver.classList.remove('review-quiz-answer-active');
+    }
+    evt.target.classList.add('review-quiz-answer-active');
+    this.activeAnsver = evt.target;
+  }
+};
+
+Review.prototype.remove = function() {
+  this.element.removeEventListener('click', this.onReviewClick);
+  this.element.parentNode.removeChild(this.element);
 };
 
 module.exports = Review;
